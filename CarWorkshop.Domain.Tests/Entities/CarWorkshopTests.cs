686b129df9dd9c56f7332045c7fc1727c44e61d8
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2026-03-07 20:51:11 +0100

    Adding Unit Test EncodedName

---
+++ CarWorkshop.Domain.Tests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using CarWorkshop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CarWorkshop.Domain.Entities.Tests
+{
+    public class CarWorkshopTests
+    {
+        [Fact()]
+        public void EncodeName_ShouldSetEncodedName()
+        {
+            //arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = "Test Workshop";
+
+            //act
+            carWorkshop.EncodeName();
+
+            //assert
+           carWorkshop.EncodedName.Should().Be("test-workshop");
+
+        }
+        [Fact()]
+        public void EncodeName_ShouldThrowException_WhenNameIsNull()
+        {   
+            //arrange
+            var carWorkshop = new CarWorkshop();
+            carWorkshop.Name = null!;
+            //act
+            Action act = () => carWorkshop.EncodeName();
+            //assert
+            act.Invoking(a => a.Invoke())
+                .Should().Throw<NullReferenceException>();
+        }
+    }
+}
