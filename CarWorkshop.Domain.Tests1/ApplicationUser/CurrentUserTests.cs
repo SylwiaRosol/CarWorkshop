ca46eca710ddb08151dc7fc8717af18519ddc1e3
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2026-03-09 22:43:01 +0100

    Adding Tests CurrentUser

---
+++ CarWorkshop.Domain.Tests1/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using CarWorkshop.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void CurrentUserTest()
+        {
+
+        }
+
+        [Fact()]
+        public void IsInRoleTest()
+        {
+
+        }
+    }
+}
