167be85193a463d50564cb61eae1ca9216e937a4
Sylwia Rosol <sylwia.rosol.programista@gmail.com>
2026-03-13 18:00:23 +0100

    Adding Validation Tests

---
+++ CarWorkshop.Application.Tests/CarWorkshopService/Commands/CreateCarWorkshopServiceCommandValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using Xunit;
+using CarWorkshop.Application.CarWorkshopService.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation.TestHelper;
+
+namespace CarWorkshop.Application.CarWorkshopService.Commands.Tests
+{
+    public class CreateCarWorkshopServiceCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand
+            {
+                Cost = "100",
+                Description = "Test description",
+                CarWorkshopEncodedName = "test-workshop"
+            };
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert
+            result.ShouldNotHaveAnyValidationErrors();
+
+        }
+
+
+
+        [Fact()]
+        public void Validate_WithInvalidCommand_ShouldHaveValidationErrors()
+        {
+            // arrange
+
+            var validator = new CreateCarWorkshopServiceCommandValidator();
+            var command = new CreateCarWorkshopServiceCommand
+            {
+                Cost = "",
+                Description = "",
+                CarWorkshopEncodedName = null
+            };
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert
+            result.ShouldHaveValidationErrorFor(c => c.Cost);
+            result.ShouldHaveValidationErrorFor(c => c.Description);
+            result.ShouldHaveValidationErrorFor(c => c.CarWorkshopEncodedName);
+
+        }
+    }
+}
